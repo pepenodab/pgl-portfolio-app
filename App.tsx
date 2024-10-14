c5862c587ae0919b595647598dd193f479e03c57
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2024-10-14 09:07:34 +0100

    refactor: meikin zings wors

--- App.tsx
+++ App.tsx
@@ -35,17 +35,17 @@ export default function App() {
             </Text>
             <ScrollView style={{padding: 10}}>
               <Text style={styles.cosasQmeGustanMuxoEstails}>Salir a pasear</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ir a la playita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Domingos de misa</Text>
+          <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
+            <Text style={styles.cosasQmeGustanMuxoEstails}>Ir a la playita</Text>
+            <Text style={styles.cosasQmeGustanMuxoEstails}>Domingos de misa</Text>
               <Text style={styles.cosasQmeGustanMuxoEstails}>La guitarrita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>El monte con lluvia</Text>
+            <Text style={styles.cosasQmeGustanMuxoEstails}>El monte con lluvia</Text>
               <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Música variadita</Text>
+     <Text style={styles.cosasQmeGustanMuxoEstails}>Música variadita</Text>
               <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
+        <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
               <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ir de cenar romántica</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Ir de cenar romántica</Text>
             </ScrollView>
             </View>
           </View>
